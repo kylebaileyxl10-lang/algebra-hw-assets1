1265f0ffb530b7f50d9c99ba4b520aa3121dd2d2
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-04-23 07:47:44 -0400

    Update vite.config.js

--- Algebra-Project/GDRWeb-main/vite.config.js
+++ Algebra-Project/GDRWeb-main/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: './', // Essential for textures to load on GitHub Pages
   server: {
     open: '/index.html',
+  },
+  build: {
+    outDir: 'dist',
   }
 });
