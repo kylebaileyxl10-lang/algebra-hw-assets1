868fa817d0acce5296414b9da10116f450586e92
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-04-23 07:39:15 -0400

    Update vite.config.js

--- Algebra-Project/GDRWeb-main/vite.config.js
+++ Algebra-Project/GDRWeb-main/vite.config.js
@@ -1,23 +1,8 @@
-import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [dts()],
-    server: {
-        open: '/index.html',
-    },
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            name: 'GDRWeb',
-            fileName: 'gdrweb',
-        },
-        /* rollupOptions: {
-            external: [],
-            output: {
-                globals: {},
-            },
-        }, */
-    },
+  base: './',
+  server: {
+    open: '/index.html',
+  }
 });
