4d8b9f271deb8505ff49b8f8372d37d29f733421
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-04-23 07:57:05 -0400

    Delete src directory

--- src/util/gdcolor.ts
+++
@@ -1,9 +0,0 @@
-import { Level } from '../level';
-import { Color } from './color';
-
-export abstract class GDColor {
-    public opacity:  number;
-    public blending: boolean;
-
-    abstract evaluate(level: Level, time: number, iterations: number): [Color, boolean];
-}
