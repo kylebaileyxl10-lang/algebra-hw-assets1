4dbff2094b8ee1e41296e7008860bd64de7a6dcc
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-04-23 07:57:36 -0400

    Update vite.config.js

--- Algebra-Project/GDRWeb-main/vite.config.js
+++ Algebra-Project/GDRWeb-main/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', 
+  base: './', // Makes sure your 3D assets load correctly
   build: {
     rollupOptions: {
-      input: 'index.html' 
+      input: 'index.html' // This kills the "Could not resolve src/index.ts" error
     }
   },
   server: {
